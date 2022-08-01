8b8c9535cf9622c280514c9f959b2e8b40866977
radmathematician <teemorg@tuta.io>
2022-08-01 18:22:46 +0100

    added jbox2d as dependency. box now floats up slowly.

--- app/src/main/java/com/example/helloworld/Loop.java
+++ app/src/main/java/com/example/helloworld/Loop.java
@@ -2,6 +2,9 @@ package com.example.helloworld;
 
 import android.graphics.Color;
 import android.util.Log;
+import org.jbox2d.collision.shapes.CircleShape;
+import org.jbox2d.common.Vec2;
+import org.jbox2d.dynamics.*;
 
 // Loop taken from: https://dewitters.com/dewitters-gameloop/
 public class Loop implements Runnable {
@@ -12,6 +15,9 @@ public class Loop implements Runnable {
     private BoundingBox levelBoundary;
     public GameObject gameObject;
     public GameObject gameObject2;
+
+    public World world;
+    public Body body;
 
     public Loop(GameSurface gameSurface) {
         this.gameSurface = gameSurface;
@@ -50,6 +56,28 @@ public class Loop implements Runnable {
         gameObject = new GameObject(0, 0, 100, 100, Color.GREEN);
         gameObject2 = new GameObject(0, 100, 100, 100, Color.RED);
         levelBoundary = new BoundingBox(0, 0, gameSurface.getWidth(), gameSurface.getHeight());
+
+        Vec2 gravity = new Vec2(0f, -10f);
+        world = new World(gravity);
+        //body definition
+        BodyDef bd = new BodyDef();
+        bd.position.set(0, 0);
+        bd.type = BodyType.DYNAMIC;
+
+        //define shape of the body.
+        CircleShape cs = new CircleShape();
+        cs.m_radius = 0.5f;
+
+        //define fixture of the body.
+        FixtureDef fd = new FixtureDef();
+        fd.shape = cs;
+        fd.density = 0.5f;
+        fd.friction = 0.3f;
+        fd.restitution = 0.5f;
+
+        //create the body and add fixture to it
+        body =  world.createBody(bd);
+        body.createFixture(fd);
     }
 
     public void updateGame(){
@@ -64,6 +92,15 @@ public class Loop implements Runnable {
         gameObject.setYPos(gameObject.getYPos() + gameObjectVelocity.getY());
 
         //PhysicsManager.applyPhysics(gameObject, levelBoundary);
+        float timeStep = 1.0f / 60.f;
+        int velocityIterations = 6;
+        int positionIterations = 2;
+        world.step(timeStep, velocityIterations, positionIterations);
+        Vec2 position = body.getPosition();
+        float angle = body.getAngle();
+
+        gameObject.setXPos(position.x);
+        gameObject.setYPos(position.y);
     }
 
     public void setGameIsRunning(boolean isRunning){
